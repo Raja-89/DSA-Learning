2500205befaff5028c816d59c0f91f97531244bf
Raja Rathour <imraja729@gmail.com>
2025-11-06 18:49:08 +0530

    recursion.cpp
    Signed-off-by: Raja Rathour <imraja729@gmail.com>

---
+++ Recursion/recursion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void print(int n)
+{
+    if (n > 0)
+    {   // Tail Recursion
+        cout << n << endl;
+        print(n - 1);
+
+        // Tail Recursion
+        print(n - 1);
+        cout << n << endl;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
